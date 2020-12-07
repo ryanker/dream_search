a37f550a530202275d9ee0c04b1f0d968592829b
Ryan <ryanker1024@gmail.com>
2020-12-07 23:02:56 +0800

    增加恢复默认功能

--- option.js
+++ option.js
@@ -1,14 +1,31 @@
 let bg = chrome.extension.getBackgroundPage()
 
 let nameEl = document.querySelectorAll('[name]')
+let saveEl = document.getElementById('save_option')
+let resetEl = document.getElementById('reset_option')
+
+// 加载
 nameEl.forEach(el => {
     el.value = (localStorage[el.name] || '').trim()
 })
 
-document.getElementById('save_option').onclick = function () {
+// 保存
+saveEl.onclick = function () {
     let obj = {}
     nameEl.forEach(el => {
         obj[el.name] = el.value.trim()
+    })
+    bg.saveOption(obj)
+    setTimeout(() => {
+        location.reload()
+    }, 300)
+}
+
+// 重置
+resetEl.onclick = function () {
+    let obj = {}
+    nameEl.forEach(el => {
+        obj[el.name] = ''
     })
     bg.saveOption(obj)
     setTimeout(() => {
